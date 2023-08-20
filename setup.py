32a97c3e97de1750c625f4d8e2865dd35a58bbac
RuQing Xu <ruqing.xu@phys.s.u-tokyo.ac.jp>
2023-08-20 20:45:58 +0900

    Rename GH Project

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='gemmfip_exo',
     version='0.0.1',
     description='Kernel generation templates for GEMMFIP',
-    url='https://github.com/xrq-phys/sgemmfip_exo/',
+    url='https://github.com/xrq-phys/gemmfip_exo/',
     author='RuQing G. Xu',
     packages=find_packages(exclude=['.github']),
     install_requires=[
